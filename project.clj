ffc55b9e3ba24558478deff2c1535444fb772eda
Kasper Jordaens <kasper.jordaens@imec.be>
2020-05-07 18:22:58 +0200

    experimenting with sockets

--- project.clj
+++ project.clj
@@ -14,6 +14,7 @@
                  [overtone "0.10.1"]
 ;                 [leipzig "0.10.0"]
                  [quil "2.2.0"]
+;                 [clj-sockets "0.1.0"]
 ;                 [glgraphics "1.0.0"]
                  ]
   :jvm-opts ^:replace []
